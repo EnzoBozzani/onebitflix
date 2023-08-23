089a67d1d52a7a80ebef26b37aa727905a004ecc
Enzo Bozzani <bozzanienzo@gmail.com>
2023-08-23 00:41:14 -0300

    feat: included pagination to get categories

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
-import { Category } from "../models/Category";
+import { categoryService } from "../services/categoryService";
+import { getPaginationParams } from "../helpers/getPaginationParams";
 
 export const categoriesController = {
     index: async (req: Request, res: Response) => {
+        const [ pageNumber, perPageNumber ] = getPaginationParams(req.query);
         try {
-            const categories = await Category.findAll({
-                order: [['position', 'ASC']]
-            });
-            return res.json(categories);
+            const paginatedCategories = await categoryService.findAllPaginated(pageNumber, perPageNumber);
+            return res.json(paginatedCategories);
         } catch (err) {
             if (err instanceof Error){
                 return res.status(400).json({ message: err.message })
@@ -15,4 +15,5 @@ export const categoriesController = {
         }
     }
 }
+
  
