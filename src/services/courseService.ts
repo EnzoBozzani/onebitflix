bdcc46856ab90d6ec2f72b3b78a1b475c2934bd4
Enzo Bozzani <bozzanienzo@gmail.com>
2023-08-23 16:26:09 -0300

    feat: added route to get top ten newest courses

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -22,5 +22,14 @@ export const courseService = {
         });
         const randomFeaturedCourses = featuredCourses.sort(() => 0.5 - Math.random());
         return randomFeaturedCourses.slice(0, 3);
+    }, 
+
+    getTopTenNewest: async () => {
+        const newestCourses = await Course.findAll({
+            attributes: ['id', 'name', 'synopsis', ['thumbnail_url', 'thumbnailUrl']], 
+            limit: 10, 
+            order: [['created_at', 'DESC']]
+        })
+        return newestCourses;
     }
 }
