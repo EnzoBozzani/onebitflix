c842c7c2caf8eb613b8c998f7ee56df856de75bc
Enzo Bozzani <bozzanienzo@gmail.com>
2023-08-23 16:14:38 -0300

    feat: added route to get 3 random featured courses

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -5,12 +5,22 @@ export const courseService = {
         const courseWithEpisodes = await Course.findByPk(id, {
             attributes: ['id', 'name', 'synopsis', ['thumbnail_url', 'thumbnailUrl']], 
             include: {
-                association: 'Episodes', 
+                association: 'episodes', 
                 attributes: ['id', 'name', 'synopsis', 'order', ['video_url', 'videoUrl'], ['seconds_long', 'secondsLong']],
                 order: [['order', 'ASC']], 
                 separate: true
             }
         })
         return courseWithEpisodes;
+    }, 
+    getRandomFeaturedCourses: async () => {
+        const featuredCourses = await Course.findAll({
+            attributes: ['id', 'name', 'synopsis', ['thumbnail_url', 'thumbnailUrl']], 
+            where: {
+                featured: true, 
+            }
+        });
+        const randomFeaturedCourses = featuredCourses.sort(() => 0.5 - Math.random());
+        return randomFeaturedCourses.slice(0, 3);
     }
 }
