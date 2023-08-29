c96cda3d097bbd4d6e1a4f9ace24d4998d47626c
Enzo Bozzani <bozzanienzo@gmail.com>
2023-08-29 15:44:48 -0300

    feat: created likes table and add route to add likes to courses

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,27 +2,33 @@ import { Category } from './Category'
 import { Course } from './Course'
 import { Episode } from './Episode'
 import { Favorite } from './Favorite'
+import { Like } from './Like'
 import { User } from './User'
 
-Category.hasMany(Course)
+Category.hasMany(Course);
 
-Course.belongsTo(Category)
-Course.hasMany(Episode)
-Course.belongsToMany(User, { through: Favorite })
-Course.hasMany(Favorite, { as: 'favoritesUsers', foreignKey: 'course_id' })
+Course.belongsTo(Category);
+Course.hasMany(Episode);
+Course.belongsToMany(User, { through: Favorite });
+Course.belongsToMany(User, { through: Like });
+Course.hasMany(Favorite, { as: 'favoritesUsers', foreignKey: 'course_id' });
 
-Episode.belongsTo(Course)
+Episode.belongsTo(Course);
 
-Favorite.belongsTo(Course)
-Favorite.belongsTo(User)
+Favorite.belongsTo(Course);
+Favorite.belongsTo(User);
 
-User.belongsToMany(Course, { through: Favorite })
-User.hasMany(Favorite, { as: 'favoritesCourses', foreignKey: 'user_id' })
+User.belongsToMany(Course, { through: Favorite });
+User.belongsToMany(Course, { through: Like });
+User.hasMany(Favorite, { as: 'favoritesCourses', foreignKey: 'user_id' });
+
+
 
 export {
     Category,
     Course,
     Episode,
     Favorite,
-    User
+    User, 
+    Like
 }
