e14555747e51b688588125249bb0fbf2bb6faa96
Enzo Bozzani <bozzanienzo@gmail.com>
2023-09-03 22:49:04 -0300

    feat: added route to get user info

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -13,5 +13,16 @@ export const usersController = {
                 return res.status(400).json({ message: err.message })
             }
         }
+    }, 
+
+    show: async (req: AuthenticatedRequest, res: Response ) => {
+        try {
+            const currentUser = req.user!;
+            return res.json(currentUser);
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
     }
 }
