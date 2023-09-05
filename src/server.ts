d1e9347a5c6d839507293cccf2bce100cd4328c3
Enzo Bozzani <bozzanienzo@gmail.com>
2023-09-05 16:50:34 -0300

    feat: added cors

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,11 @@ import express from 'express';
 import { sequelize } from './database';
 import { adminJs, adminJsRouter } from './admin';
 import { router } from './routes';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors())
 
 app.use(adminJs.options.rootPath, adminJsRouter);
 
